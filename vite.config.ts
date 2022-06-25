828f1b9a40e65b118660233f92399844ff4d3c18
rosong <rousong111@gmail.com>
2022-06-25 14:03:25 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
+
+const fileName = {
+    es: "petx.mjs",
+    cjs: "petx.cjs",
+    iife: "petx.iife.js",
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/test/setup.ts",
+    },
+    base: "./",
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.tsx"),
+            name: "petx",
+            formats: ["es", "cjs", "iife"],
+            fileName: (format) => fileName[format],
+        },
+        rollupOptions: {
+            external: ["react"],
+        },
+    },
+});
